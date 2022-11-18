fc49d367419a7018ee3fff86c3ea79081dfa5588
Alaa Aqeel <alaa.21.iraq@gmail.com>
2022-11-18 17:25:23 +0300

    new file:   api/tests/Feature/UsersTest.php

---
+++ api/tests/Feature/UsersTest.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+
+class UsersTest extends TestCase
+{
+    /**
+     * Test create new user 
+     *
+     * @return void
+     */
+    public function test_create_new_user()
+    {
+
+        $fakeUser = User::factory()->raw(["password" => "Test_test%%(99"]);
+
+        $response = $this->postJson(route('users.store'), $fakeUser);
+
+        $response
+            ->assertStatus(201)
+            ->assertJsonPath("message", __("messages.created"))
+            ->assertJsonPath("data.is_super", 0)
+            ->assertJsonPath("data.is_active", 1);
+    }
+
+    /**
+     * Test get users 
+     * 
+     * @return void 
+     */
+    public function test_get_all_users()
+    {
+        User::factory(20)->create();
+
+        $response = $this->getJson(route('users.index'));
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonCount(10, "data"); // pagination limit 10 
+    }
+
+
+    /**
+     * Test get one user 
+     * 
+     * @return void 
+     */
+    public function test_get_one_user()
+    {
+        $user = User::factory(20)->create()->first();
+
+        $response = $this->getJson(route('users.show', ["user" => $user->id ]));
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonPath("data.name", $user->name); 
+    }
+
+
+    /**
+     * Test update user 
+     * 
+     * @return void 
+     */
+    public function test_update_user()
+    {
+        $user = User::factory()->create();
+        $raw = User::factory()->raw();
+        
+        $route = route('users.update', ["user" => $user->id]);
+
+        $response = $this->putJson($route, [
+            "name" => $raw['name'],
+            "email" => $raw['email']
+        ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonPath("data.name", $raw['name'])
+            ->assertJsonPath("data.email", $raw['email']); 
+    }
+
+
+    /**
+     * Test update password 
+     * 
+     * @return void 
+     */
+    public function test_update_password_user()
+    {
+        $user = User::factory()->create();
+        
+        $route = route('users.update', ["user" => $user->id]);
+
+        $response = $this->putJson($route, [
+            "name" => $user->name,
+            "email" => $user->email,
+            "password" => "tesT_pass2022!",
+        ]);
+
+        $response->assertStatus(200); 
+
+        $this->assertTrue(Auth::attempt([
+            "email" => $user->email,
+            "password" => "tesT_pass2022!",
+        ]));
+
+    }
+
+
+    /**
+     * Test delete user 
+     * 
+     * @return void 
+     */
+    public function test_delete_user()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->deleteJson(route('users.update', ["user" => $user->id]));
+
+        $response->assertStatus(204); 
+    }
+}
